672420ff9fdb658d1b5178e214df60fc9ee555e8
jhills5821 <44476310+jhills5821@users.noreply.github.com>
2019-03-31 15:02:32 -0700

    fineshed level 2

--- static/js/app.js
+++ static/js/app.js
@@ -1,12 +1,9 @@
-// from data.js
 var tableData = data;
 var tbody = d3.select("tbody");
 
 tableData.forEach(function(ufoSigtings) {
-    //console.log(ufoSigtings);
     var row = tbody.append("tr");
     Object.entries(ufoSigtings).forEach(function([key, value]) {
-      //console.log(key, value);
       var cell = tbody.append("td");
       cell.text(value);
     });
@@ -17,49 +14,39 @@ var filter = d3.select("#filter-btn");
 
 filter.on("click", function() {
 
-    // Prevent the page from refreshing
+
     d3.event.preventDefault();
     
-    // Select the input element and get the raw HTML node
     var inputElementdate = d3.select("#datetime");
     var inputElementcity = d3.select("#city");
     var inputElementstate = d3.select("#state");
     var inputElementcountry = d3.select("#country");
     var inputElementshape = d3.select("#shape");
 
-    // Get the value property of the input element
     var inputDate = inputElementdate.property("value");
     var inputCity = inputElementcity.property("value");
     var inputState = inputElementstate.property("value");
     var inputCountry = inputElementcountry.property("value");
     var inputShape = inputElementshape.property("value");
 
-    //console.log(inputValue);
+    var filteredData = tableData.filter(tableData => tableData.datetime === inputDate|| 
+        tableData.city === inputCity ||
+        tableData.state === inputState || 
+        tableData.country === inputCountry || 
+        tableData.shape === inputShape);
 
-    var filteredData = tableData.filter(tableData => inputDate === tableData.datetime || 
-        inputCity === tableData.city ||
-        inputState === tableData.state || 
-        inputCountry === tableData.country || 
-        inputShape === tableData.shape);
-
-    console.log(filteredData);
-
-    //tableData.forEach(function(items) {
-        //console.log(ufoSigtings);
-      //  var removeRows = tbody.remove("tr");
-    //});
-    //document.getElementById("ufo-table").innerHTML=filteredData;
+        tableData.forEach(function(ufoSigtings){
+        var removeRows = tbody.remove("tr");
+    });
     
-    filteredData.forEach(function(ufoSigtings) {
-       //console.log(ufoSigtings);
-        var row = tbody.append("tr");
-        Object.entries(ufoSigtings).forEach(function([key, value]) {
-            //console.log(key, value);
-            var cell = tbody.append("td");
+    filteredData.forEach(function(ufoSightings) {
+        var row = d3.select("table").append("tr");
+        Object.entries(ufoSightings).forEach(function([key, value]) {
+            var cell = d3.select("table").append("td");
             cell.text(value);
         });
    });
-
+   console.log(filteredData);
     
 });
 
