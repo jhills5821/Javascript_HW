c92ccbfa53308bf71c273956fbdae237147afed3
jhills5821 <44476310+jhills5821@users.noreply.github.com>
2019-04-01 18:50:21 -0700

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,5 @@
-var tableData = data;
-var tbody = d3.select("tbody");
+let tableData = data;
+let tbody = d3.select("tbody");
 
 tableData.forEach(function(ufoSigtings) {
     var row = tbody.append("tr");
@@ -10,39 +10,39 @@ tableData.forEach(function(ufoSigtings) {
 });
 
 
-var filter = d3.select("#filter-btn");
+let filter = d3.select("#filter-btn");
 
 filter.on("click", function() {
 
 
     d3.event.preventDefault();
     
-    var inputElementdate = d3.select("#datetime");
-    var inputElementcity = d3.select("#city");
-    var inputElementstate = d3.select("#state");
-    var inputElementcountry = d3.select("#country");
-    var inputElementshape = d3.select("#shape");
+    let inputElementdate = d3.select("#datetime");
+    let inputElementcity = d3.select("#city");
+    let inputElementstate = d3.select("#state");
+    let inputElementcountry = d3.select("#country");
+    let inputElementshape = d3.select("#shape");
 
-    var inputDate = inputElementdate.property("value");
-    var inputCity = inputElementcity.property("value");
-    var inputState = inputElementstate.property("value");
-    var inputCountry = inputElementcountry.property("value");
-    var inputShape = inputElementshape.property("value");
+    let inputDate = inputElementdate.property("value");
+    let inputCity = inputElementcity.property("value");
+    let inputState = inputElementstate.property("value");
+    let inputCountry = inputElementcountry.property("value");
+    let inputShape = inputElementshape.property("value");
 
-    var filteredData = tableData.filter(tableData => tableData.datetime === inputDate|| 
+    let filteredData = tableData.filter(tableData => tableData.datetime === inputDate|| 
         tableData.city === inputCity ||
         tableData.state === inputState || 
         tableData.country === inputCountry || 
         tableData.shape === inputShape);
 
         tableData.forEach(function(ufoSigtings){
-        var removeRows = tbody.remove("tr");
+        let removeRows = tbody.remove("tr");
     });
     
     filteredData.forEach(function(ufoSightings) {
-        var row = d3.select("table").append("tr");
+        let row = d3.select("table").append("tr");
         Object.entries(ufoSightings).forEach(function([key, value]) {
-            var cell = d3.select("table").append("td");
+            let cell = d3.select("table").append("td");
             cell.text(value);
         });
    });
